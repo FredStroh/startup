bfdaffbe6609ad176d2ebcc240164ba53cfa8e9e
FredStroh <F.Stroh@fz-juelich.de>
2018-10-16 14:50:42 +0200

    First py prog

---
+++ hw.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+def helloworld():
+    """
+    """
+    return "Hello World!"
+
+# This block will not be executed if loaded as a module ...
+if __name__ == "__main__":
+    pass
+
+print(helloworld())
+
